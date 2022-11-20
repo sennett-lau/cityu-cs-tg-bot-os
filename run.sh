a7f375fab918f16411fec1752ea7ffad4db46cf5
Sennett Lau <laub1199@gmail.com>
2022-11-21 00:27:35 +0800

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,1 @@
-#! /bin/bash
-
-# Run docker file
-docker run -d --name cityu-cs-tg-bot cityu-cs-tg-bot --env-file "$(dirname "$0")/app/.env.dev"
+docker run -d --name cityu-cs-tg-bot cityu-cs-tg-bot
