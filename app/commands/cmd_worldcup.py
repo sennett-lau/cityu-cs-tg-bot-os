515b447e1d846404a6ab091cc164883839e9da4a
Eva Chan <chingwa0812@gmail.com>
2022-11-27 21:49:40 +0800

    Change the worldcup implementation

--- app/commands/cmd_worldcup.py
+++ app/commands/cmd_worldcup.py
@@ -16,69 +16,64 @@ matchStatus = {
 groupLetter = re.compile('^[a-hA-H]$').search
 
 def worldcup(update, context):
+
     if not context.args or context.args[0] == 'help':
-        worldcup_help(update, context)
+        message = worldcup_help()
     elif context.args[0] == 'tdy':
         jwt = wc_api_login()
-        worldcup_tdy(update, context, jwt)
+        message = worldcup_tdy(jwt)
     elif context.args[0] == 'ytd':
         jwt = wc_api_login()
-        worldcup_ytd(update, context, jwt)
+        message = worldcup_ytd(jwt)
     elif context.args[0] == 'tmr':
         jwt = wc_api_login()
-        worldcup_tmr(update, context, jwt)
+        message = worldcup_tmr(jwt)
     elif context.args[0] == 'standings':
         letter = context.args[1].upper()
         if not bool(groupLetter(letter)):
-            update.message.reply_text('Invalid Input, Please input /worldcup standings [Group]. Group A to H')
-            return
-        jwt = wc_api_login()
-        worldcup_standing(update, context, jwt, letter)
+            message = "Invalid Input, Please input /worldcup standings [Group]. Group A to H"
+        else:
+            jwt = wc_api_login()
+            message = worldcup_standing(jwt, letter)
     else:
-        worldcup_error(update, context)
+        message = worldcup_error()
+    update.message.reply_text(message)
 
-def worldcup_help(update, context):
+def worldcup_help():
     text = '⚽World Cup 2022 Functions are now LIVE!🏆\n\n'
     text += '/worldcup tdy - Get today\'s matches\n'
     text += '/worldcup ytd - Get yesterday\'s matches\n'
     text += '/worldcup tmr - Get tomorrow\'s matches\n'
     text += '/worldcup standings [Group] - Get current standings, Group A to H (e.g. /worldcup standings H)'
-    update.message.reply_text(text)
+    return text
 
-def worldcup_error(update, context):
-    update.message.reply_text("Unknown command")
+def worldcup_error():
+    return "Unknown command"
 
-def worldcup_tdy(update, context, jwt):
+def worldcup_tdy(jwt):
     reply = 'Today\'s matches:\n\n'
 
     today = datetime.now().strftime('%m/%d/%Y')
 
-    reply = worldcup_get_match_by_date(jwt, reply, today)
+    return worldcup_get_match_by_date(jwt, reply, today)
 
-    update.message.reply_text(reply)
-
-def worldcup_ytd(update, context, jwt):
+def worldcup_ytd(jwt):
     reply = 'Yesterday\'s matches:\n\n'
 
     ytd = datetime.now()
     ytd = ytd - timedelta(hours=24)
     ytd = ytd.strftime('%m/%d/%Y')
 
-    reply = worldcup_get_match_by_date(jwt, reply, ytd)
+    return worldcup_get_match_by_date(jwt, reply, ytd)
 
-    update.message.reply_text(reply)
-
-def worldcup_tmr(update, context, jwt):
+def worldcup_tmr(jwt):
     reply = 'Tomorrow\'s matches:\n\n'
 
     tmr = datetime.now()
     tmr = tmr + timedelta(hours=24)
     tmr = tmr.strftime('%m/%d/%Y')
 
-
-    reply = worldcup_get_match_by_date(jwt, reply, tmr)
-
-    update.message.reply_text(reply)
+    return worldcup_get_match_by_date(jwt, reply, tmr)
 
 def worldcup_get_match_by_date(jwt, reply, date):
     matches = wc_api_get_match_by_date(jwt, date)
@@ -96,11 +91,11 @@ def worldcup_get_match_by_date(jwt, reply, date):
 
     return reply
 
-def worldcup_standing(update, context, jwt, letter):
+def worldcup_standing(jwt, letter):
     groupStandings = wc_api_get_standings(jwt, letter)
     reply = 'Current standings:\n\n'
-
     reply += 'Group: ' + letter + '\n\n'
+
     teams = groupStandings[0]['teams']
     for team in teams:
         reply += get_flag(team['name_en']) + team['name_en'] + '\n'
@@ -113,8 +108,6 @@ def worldcup_standing(update, context, jwt, letter):
         reply += 'Goals difference: ' + team['gd'] + '\n'
         reply += 'Drawn: ' + team['d'] + '\n'
         reply += '\n'
-    reply += '\n\n'
 
-    update.message.reply_text(reply)
-
+    return reply
 
